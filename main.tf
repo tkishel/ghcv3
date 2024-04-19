ff87bcdf893b88c14af703e9c691ec3699db8529
Thomas Kishel <tkishel@gmail.com>
2024-04-19 13:17:06 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
-resource "aws_s3_bucket" "wiz" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "my-example_bucket"
   versioning {
     enabled = true
   }
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+  region        = "us-west-2"
 }
