61445b187b62aecfaa3e6b8a5dacfee75d21bdc6
Tom Kishel <tkishel@gmail.com>
2025-02-08 09:05:04 -0800

    xxx

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
+
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "my-example_bucket"
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_instance" "example_server" {
-  ami           = "ami-04e914639d0cca79a"
-  instance_type = "t2.micro"
-  region        = "us-west-2"
+  block_public_acls = false
 }
